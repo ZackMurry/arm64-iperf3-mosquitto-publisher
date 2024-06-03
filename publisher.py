6b051f1dd8aba65c970a59270610a653e150fec6
Zack Murry <zackk.murryy@gmail.com>
2024-06-03 13:59:08 -0500

    Remove pub for testing

--- publisher.py
+++ publisher.py
@@ -1,22 +1,23 @@
-import paho.mqtt.client as mqtt
-from time import sleep
-import os
-
-def on_connect(client, userdata, flags, reason_code, properties):
-	print(f"Connected with code {reason_code}")
-
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.on_connect = on_connect
-broker_ip = os.environ("BROKER_IP")
-client.connect(broker_ip)
-
-sleep(5)
-client.loop()
-print("Ready!")
-
-for i in range(10):
-	client.publish("bandwidth", i, qos=2)
-
-client.disconnect()
-client.loop_stop()
-
+#import paho.mqtt.client as mqtt
+#from time import sleep
+#import os
+#
+print("Hello, world!")
+#def on_connect(client, userdata, flags, reason_code, properties):
+#	print(f"Connected with code {reason_code}")
+#
+#client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+#client.on_connect = on_connect
+#broker_ip = os.environ("BROKER_IP")
+#client.connect(broker_ip)
+#
+#sleep(5)
+#client.loop()
+#print("Ready!")
+#
+#for i in range(10):
+#	client.publish("bandwidth", i, qos=2)
+#
+#client.disconnect()
+#client.loop_stop()
+#
