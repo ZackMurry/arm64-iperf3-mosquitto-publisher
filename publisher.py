d3115ad64ad0918695b5868d1002e04e6191bd8d
Zack Murry <zackk.murryy@gmail.com>
2024-06-03 14:40:40 -0500

    Restore publisher

--- publisher.py
+++ publisher.py
@@ -1,25 +1,22 @@
-#import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 from time import sleep
-#import os
-#
-while true:
-	print("Hello, world!")
-	sleep(5)
-#def on_connect(client, userdata, flags, reason_code, properties):
-#	print(f"Connected with code {reason_code}")
-#
-#client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-#client.on_connect = on_connect
-#broker_ip = os.environ("BROKER_IP")
-#client.connect(broker_ip)
-#
-#sleep(5)
-#client.loop()
-#print("Ready!")
-#
-#for i in range(10):
-#	client.publish("bandwidth", i, qos=2)
-#
-#client.disconnect()
-#client.loop_stop()
-#
+import os
+
+def on_connect(client, userdata, flags, reason_code, properties):
+	print(f"Connected with code {reason_code}")
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+broker_ip = os.environ("BROKER_IP")
+client.connect(broker_ip)
+
+sleep(5)
+client.loop()
+print("Ready!")
+
+for i in range(10):
+	client.publish("bandwidth", i, qos=2)
+
+client.disconnect()
+client.loop_stop()
+
